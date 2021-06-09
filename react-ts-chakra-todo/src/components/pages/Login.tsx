accb8916dbb845341e9715a9e3d3bc94b2747b7d
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-06-09 09:01:02 +0900

    ログイン画面モーダル作成

--- react-ts-chakra-todo/src/components/pages/Login.tsx
+++ react-ts-chakra-todo/src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
-import {memo, VFC} from "react";
+import { memo, VFC } from "react";
+import {
+  Box,
+  Button,
+  Stack,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページです</p>
-})
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing="6" py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+});
