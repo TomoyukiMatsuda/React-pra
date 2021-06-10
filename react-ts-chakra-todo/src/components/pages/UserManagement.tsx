3c579b6a80cadfc7cdd27f1949806feace999e10
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-06-11 08:34:29 +0900

    ユーザー一覧のアイテムを仮作成

--- react-ts-chakra-todo/src/components/pages/UserManagement.tsx
+++ react-ts-chakra-todo/src/components/pages/UserManagement.tsx
@@ -1,5 +1,17 @@
-import {memo, VFC} from "react";
+import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>
-})
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="まつだくん"
+          fullName="matsudatomo"
+        />
+      </WrapItem>
+    </Wrap>
+  );
+});
