248a68a0df0a989dc4fa702348a1db79d6ef2f07
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-06-22 07:55:07 +0900

    子コンポーネントを作成

--- re-rendering/src/App.js
+++ re-rendering/src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import {ChildArea} from "./ChildArea";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
   console.log('レンダリング');
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
 
-  const onClickCountUp = () => {
-    setCount(count + 1)
-  }
   return (
     <div className="App">
       <h1>再レンダリングを学ぶ</h1>
-
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br/>
+      <br/>
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
---
+++ re-rendering/src/ChildArea.jsx
@@ -0,0 +1,18 @@
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = (props) => {
+  const { open } = props;
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  )
+}
