47e5a50c54cd52d50f571e95462e94b4a7315a66
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-06-22 07:39:20 +0900

    カウントアップサンプル

--- re-rendering/src/App.js
+++ re-rendering/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  console.log('レンダリング');
+
+  const onClickCountUp = () => {
+    setCount(count + 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>再レンダリングを学ぶ</h1>
+
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
     </div>
   );
 }
