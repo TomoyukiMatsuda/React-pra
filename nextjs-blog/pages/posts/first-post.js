ce81ac85458655e268c63b5729ab3ffec40479ce
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-06-25 08:24:53 +0900

    Linkコンポーネントによるページ遷移

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,6 +1,15 @@
-
+import Link from 'next/link'
 // default export しないとだめ
 // ディレクトリ/ファイル名 がパスになる 今回だと→ 「posts/first-post」
 export default function FirstPost() {
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
 }
