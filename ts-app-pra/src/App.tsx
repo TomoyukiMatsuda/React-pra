1399d73ea216e202bb3415eb9c2ead3a6a7a80b7
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-26 09:10:07 +0900

    Intersection Typesを学ぶ

--- ts-app-pra/src/App.tsx
+++ ts-app-pra/src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Intersection Types
+type Profile = {
+  age: number;
+  city: string;
+}
+
+type Login = {
+  username: string;
+  password: string;
+}
+
+type User = Profile & Login;
+const user1: User = {
+  age: 30,
+  city: "Nagano",
+  username: "Matsuda",
+  password: "xxxxyyyy"
+}
+
+console.log(user1);     
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
