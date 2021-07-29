98e74a08b406f00750c92af843291b5f2c3cbd33
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-29 11:40:29 +0900

    firebase 利用設定を追加

---
+++ firebase-todo/src/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
+export const db = firebaseApp.firestore();
+export const auth = firebase.auth();
