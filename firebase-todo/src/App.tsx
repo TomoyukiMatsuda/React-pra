be1b267049ebf26a34de565fdd95d7133cbb4493
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-30 07:47:24 +0900

    firebase にタスクを追加

--- firebase-todo/src/App.tsx
+++ firebase-todo/src/App.tsx
@@ -1,10 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import {FormControl, TextField} from "@material-ui/core";
+import React, {MouseEventHandler, useEffect, useState} from 'react';
 import './App.css';
 import { db } from "./firebase";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 
 const App: React.FC = () => {
   // firebaseから取得してくるDBのtasksを初期化、stateとして持っておく
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
+
   // 最初にレンダリングするタイミングでデータを取得してきてtasksにセットする
   useEffect(() => {
     const unSubscribe =
@@ -18,9 +22,30 @@ const App: React.FC = () => {
     // アンマウントするときにリスナー的な機能を解除する
     return () => unSubscribe();
   }, []);
+  // todo 引数eはなんのため？
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: input })
+    setInput("");
+  };
 
   return (
     <div className="App">
+      <h1>Todo App React/Firebase</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="新しいタスクを作成"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask} >
+        <AddToPhotosIcon />
+      </button>
       {tasks.map((task) => (
         <h3>タイトル：{task.title}</h3>
       ))}
