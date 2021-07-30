f40776f8a4567976cc1ce446ae86ea878a597383
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-30 20:05:03 +0900

    ReactRouterでコンポーネントを切り替え

---
+++ firebase-todo/src/Login.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h1>ログインコンポーネント</h1>
+    </div>
+  );
+}
+
+export default Login;
--- firebase-todo/src/index.tsx
+++ firebase-todo/src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter } from "react-router-dom";
+import Login from "./Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+      </>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
