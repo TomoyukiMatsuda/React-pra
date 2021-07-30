d9f05c25aa6f58931398f1e8c761f449a41d5e8e
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-30 09:58:51 +0900

    タスクのedit, delete 機能

--- firebase-todo/src/TaskItem.tsx
+++ firebase-todo/src/TaskItem.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import {ListItem} from "@material-ui/core";
-
+import { db } from "./firebase";
+import React, {useState} from "react";
+import {Grid, ListItem, TextField} from "@material-ui/core";
+import {DeleteOutlined, EditOutlined} from "@material-ui/icons";
 
 interface Props {
   id: string;
@@ -8,10 +9,37 @@ interface Props {
 }
 
 const TaskItem: React.FC<Props> = (props) => {
+  // タスクタイトルのstateを持っておく（初期値をもとのタイトル）
+  const [title, setTitle] = useState(props.title);
+  const editTask = () => {
+    db.collection("tasks").doc(props.id).set({ title: title }, { merge: true });
+  };
+  const deleteTask = () => {
+    db.collection("tasks").doc(props.id).delete();
+  };
+
   return (
     <div>
       <ListItem>
         <h2>□ {props.title}</h2>
+        <Grid container justify={"flex-end"}>
+          <TextField
+            InputLabelProps={{
+              shrink: true,
+            }}
+            label="編集タスク"
+            value={title}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setTitle(e.target.value)
+            }
+          />
+        </Grid>
+        <button onClick={editTask}>
+          <EditOutlined />
+        </button>
+        <button onClick={deleteTask}>
+          <DeleteOutlined />
+        </button>
       </ListItem>
     </div>
   )
