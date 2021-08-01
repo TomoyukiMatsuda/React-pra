94b718076dc4f09f4fd859fe5a9f85fe6d03b316
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-02 08:59:46 +0900

    ツイート一覧を取得して仮表示

--- twitter-clone-fb/src/components/Feed.tsx
+++ twitter-clone-fb/src/components/Feed.tsx
@@ -1,14 +1,47 @@
-import React from "react";
-import { auth } from "../firebase";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 import TweetInput from "./TweetInput";
 import styles from "./Feed.module.css";
+import firebase from "firebase";
 
 const Feed: React.FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      avatar: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+  useEffect(() => {
+    const unSubscribe = db
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        );
+      });
+
+    // リスナー（subscribe）を解除
+    return () => unSubscribe();
+  }, []);
+
   return (
     <div className={styles.feed}>
-      Feed
       <TweetInput />
-      <button onClick={() => auth.signOut()}>Logout</button>
+      {posts.map((post) => (
+        <h3>{post.text}</h3>
+      ))}
     </div>
   );
 };
