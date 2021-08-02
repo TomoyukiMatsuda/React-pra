be86711f95cb475a38ac9e3ba3862f4d1328b659
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-03 08:11:03 +0900

    投稿データをフィードに表示

--- twitter-clone-fb/src/components/Post.tsx
+++ twitter-clone-fb/src/components/Post.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { db } from "../firebase";
 import firebase from "firebase/app";
+import { Avatar } from "@material-ui/core";
+import styles from "./Post.module.css";
 
 interface Props {
   postId: string;
@@ -11,8 +13,29 @@ interface Props {
   username: string;
 }
 
+// Props で投稿データを受け取って表示
 const Post: React.FC<Props> = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <Avatar src={props.avatar} />
+      </div>
+      <div className={styles.post_body}>
+        <h3>
+          <span className={styles.post_headerUser}>@{props.username}</span>
+          <span>{new Date(props.timestamp?.toDate()).toLocaleString()}</span>
+        </h3>
+      </div>
+      <div className={styles.post_tweet}>
+        <p>{props.text}</p>
+      </div>
+      {props.image && (
+        <div className={styles.post_tweetImage}>
+          <img src={props.image} alt="tweet" />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Post;
