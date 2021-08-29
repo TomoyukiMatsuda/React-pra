3105055ba2cf2bd7b48ab84a66bd721653f6a30d
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-29 20:17:50 +0900

    検索フォームをコンポーネント化

---
+++ next-sample/src/components/SearchForm/index.tsx
@@ -0,0 +1,42 @@
+import React, { Dispatch, FormEvent, SetStateAction, useState } from "react";
+
+interface Props {
+  fetchArticles: (
+    e: FormEvent<HTMLFormElement>,
+    searchText: string,
+    setSearchText: Dispatch<SetStateAction<string>>
+  ) => void;
+}
+
+export const SearchForm: React.VFC<Props> = (props) => {
+  const [searchText, setSearchText] = useState<string>("");
+  const buttonColor = searchText
+    ? "bg-blue-700 hover:bg-blue-500"
+    : "bg-gray-300";
+
+  return (
+    <div>
+      <form
+        className="p-12"
+        onSubmit={(e) => props.fetchArticles(e, searchText, setSearchText)}
+      >
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Qiita 記事検索キーワードを入力
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="例：React"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
+          className={`${buttonColor} text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+          type="submit"
+          disabled={!searchText}
+        >
+          検索
+        </button>
+      </form>
+    </div>
+  );
+};
--- next-sample/src/pages/index.tsx
+++ next-sample/src/pages/index.tsx
@@ -1,40 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { useListQiitaArticles } from "../hooks/useListQiitaArticles";
 import { ArticleList } from "../components/ArticleList";
+import { SearchForm } from "../components/SearchForm";
 
 const Home: React.VFC = () => {
   const { articles, errorMessage, isLoading, isEmpty, fetchArticles } =
     useListQiitaArticles();
-  const [searchText, setSearchText] = useState<string>("");
-  const buttonColor = searchText
-    ? "bg-blue-700 hover:bg-blue-500"
-    : "bg-gray-300";
 
   return (
     <div className="max-w-5xl my-0 mx-auto">
-      <div>
-        <form
-          className="p-12"
-          onSubmit={(e) => fetchArticles(e, searchText, setSearchText)}
-        >
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Qiita 記事検索キーワードを入力
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="例：React"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <button
-            className={`${buttonColor} text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
-            type="submit"
-            disabled={!searchText}
-          >
-            検索
-          </button>
-        </form>
-      </div>
+      <SearchForm fetchArticles={fetchArticles} />
       <ArticleList
         articles={articles}
         errorMessage={errorMessage}
