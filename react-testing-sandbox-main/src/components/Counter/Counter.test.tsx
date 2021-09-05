f6dda34f4efd1f8ec5427614246b99f34655864f
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-09-06 08:51:32 +0900

    Counterコンポーネントのテストを実施

--- react-testing-sandbox-main/src/components/Counter/Counter.test.tsx
+++ react-testing-sandbox-main/src/components/Counter/Counter.test.tsx
@@ -2,19 +2,27 @@ import { cleanup, fireEvent, render, screen } from "@testing-library/react"
 
 import { Counter } from "./"
 
+// "Counter"はテストグループ名、"render"と"click:count"はテストケース
 describe("Counter", () => {
+  // テストケース終了ごとにReactコンポーネントを画面からunmountするためのクリーンナップ用ヘルパー関数
   afterEach(() => {
     cleanup()
   })
   test("render", () => {
-    const { asFragment } = render(<Counter />)
+    const { asFragment, getByText } = render(<Counter />)
     expect(asFragment()).toMatchSnapshot()
+    // 最初のレンダリングでテキストの表示が "Count: 0" であることをテスト
+    expect(getByText("Count: 0"))
   })
   test("click:count", () => {
     render(<Counter />)
+    // "Increment"のテキストが入ったbuttonを取得
     const button = screen.getByText("Increment")
-    fireEvent.click(button)
-    fireEvent.click(button)
-    screen.getByText("Count: 2")
+    // 上記で取得したボタンを5回クリックすると
+    for (let i = 0; i < 5; i++) {
+      fireEvent.click(button)
+    }
+    // 画面のテキストには Count: 5 というテキストが表示される
+    screen.getByText("Count: 5")
   })
 })
--- react-testing-sandbox-main/src/components/Counter/Counter.tsx
+++ react-testing-sandbox-main/src/components/Counter/Counter.tsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react"
 
 export function Counter() {
