07ff2d34a0ecdd7900861c9f0be3a56b4afef748
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-09-09 08:20:48 +0900

    selector を作成

---
+++ recoil-sample/src/SearchedTodoListSelector.ts
@@ -0,0 +1,18 @@
+import { selector } from "recoil";
+import { Todo } from "./Todo";
+import { todoListState } from "./TodoListAtom";
+import { searchTextFormState } from "./SearchTextFormAtom";
+
+export const searchedTodoListSelector = selector<Todo[]>({
+  key: "searchedTodoListSelector",
+  get: ({ get }) => {
+    // get(宣言したAtom) で最新の値を取得
+    const todoList: Todo[] = get(todoListState); // タスク一覧を取得
+    const searchText: string = get(searchTextFormState); // 検索テキストを取得
+    // searchTextがあれば：検索テキストに該当するリストを返す
+    // searchTextがなければ：そのまま全てのリストを返す
+    return searchText
+      ? todoList.filter((todo) => todo.title.includes(searchText))
+      : todoList;
+  },
+});
