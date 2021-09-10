50adc77e328ec82cdb3355f3cad9db5484cb1340
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-09-11 08:23:45 +0900

    qiita記事リストを保持するatomを作成

---
+++ next-sample/src/atoms/searchArticleListAtom.ts
@@ -0,0 +1,9 @@
+import { atom } from "recoil";
+import { QiitaItem } from "../types/QiitaItem";
+
+// todo: 型定義修正したいかも
+// searchArticleList（Qiita記事リスト）のAtom（データストア）だよという宣言
+export const searchArticleListAtom = atom<QiitaItem[]>({
+  key: "searchArticleList", // プロジェクトで固有のキー
+  default: [], // 初期値
+});
