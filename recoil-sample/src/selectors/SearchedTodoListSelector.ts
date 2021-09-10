3559cc5a09959b54be856ef3d9287d567793374e
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-09-10 19:00:11 +0900

    selector コメント追加

--- recoil-sample/src/selectors/SearchedTodoListSelector.ts
+++ recoil-sample/src/selectors/SearchedTodoListSelector.ts
@@ -3,6 +3,8 @@ import { Todo } from "../types/Todo";
 import { todoListState } from "../atoms/TodoListAtom";
 import { searchTextFormState } from "../atoms/SearchTextFormAtom";
 
+// todo: selectorFamily との違いを確認する、ただのselectorだと引数を取れないのかも
+//   selectorFamily<T, P> T：返り値型, P:引数型 っぽい
 export const searchedTodoListSelector = selector<Todo[]>({
   key: "searchedTodoListSelector",
   get: ({ get }) => {
@@ -11,6 +13,7 @@ export const searchedTodoListSelector = selector<Todo[]>({
     const searchText: string = get(searchTextFormState); // 検索テキストを取得
     // searchTextがあれば：検索テキストに該当するリストを返す
     // searchTextがなければ：そのまま全てのリストを返す
+    // このようにAtomから直接データを取得するのではなく、Selector で加工したデータを取得したいときに利用、データの中間層だと理解
     return searchText
       ? todoList.filter((todo) => todo.title.includes(searchText))
       : todoList;
