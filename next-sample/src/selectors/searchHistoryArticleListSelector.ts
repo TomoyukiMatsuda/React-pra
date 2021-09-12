2e8d5fdd796a02298fd555f125fbd58cb9b0d2fa
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-09-12 12:57:16 +0900

    LGTM数で絞り込みグローバルステートで管理

--- next-sample/src/selectors/searchHistoryArticleListSelector.ts
+++ next-sample/src/selectors/searchHistoryArticleListSelector.ts
@@ -1,25 +1,22 @@
 import { DefaultValue, selectorFamily } from "recoil";
 import { QiitaItem } from "../types/QiitaItem";
 import { searchHistoryArticleListState } from "../atoms/searchHistoryArticleListAtom";
+import { minLikesCountState } from "../atoms/minLikesCountAtom";
 
 // atomFamilyとselectorFamilyを利用することによって、引数key毎にデータをatomに格納できる
 // searchArticleList（Qiita記事リスト）のSelector（データ整形所）だよという宣言
 export const searchHistoryArticleListSelector = selectorFamily<
   QiitaItem[],
   string
-  //{ key: string; minLikesCount: number }
 >({
   key: "searchHistoryArticleListSelector",
   get:
-    //( key, minLikesCount = 0 }) =>
-
-      (key) =>
-      ({ get }) => {
-        return get(searchHistoryArticleListState(key));
-        // return get(searchHistoryArticleListState(key)).filter((item) => {
-        //   return item.likes_count >= minLikesCount;
-        // });
-      },
+    (key) =>
+    ({ get }) => {
+      return get(searchHistoryArticleListState(key)).filter((item) => {
+        return item.likes_count >= get(minLikesCountState);
+      });
+    },
   // Atomに直接セットすることも可能だけどselectorからセットするようにする
   set:
     (key) =>
