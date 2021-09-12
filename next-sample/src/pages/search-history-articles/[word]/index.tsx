967b849e121dea5c73db45b137bd3b0f337fa3ee
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-09-12 13:37:08 +0900

    LGTMカウントリセット機能

--- next-sample/src/pages/search-history-articles/[word]/index.tsx
+++ next-sample/src/pages/search-history-articles/[word]/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useRouter } from "next/router";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
 import { searchHistoryArticleListSelector } from "../../../selectors/searchHistoryArticleListSelector";
 import { ArticleItem } from "../../../components/ArticleList/ArticleItem";
 import { minLikesCountState } from "../../../atoms/minLikesCountAtom";
@@ -9,12 +9,25 @@ import { minLikesCountState } from "../../../atoms/minLikesCountAtom";
 const ArticlesBySearchHistoryWord: React.VFC = () => {
   const router = useRouter();
   const searchWord = (router.query.word as string) || "";
-  // pathから取得した検索ワードに紐づくステートを取得
   const articleList = useRecoilValue(
     searchHistoryArticleListSelector(searchWord)
   );
   const [minLikesCount, setMinLikeCount] = useRecoilState(minLikesCountState);
+  const resetMinLikesCount = useResetRecoilState(minLikesCountState);
+  const buttonColor =
+    minLikesCount !== 0
+      ? "bg-red-400 hover:bg-red-300" // フォーム入力有：ブルー
+      : "bg-gray-300"; // フォーム入力無：グレー
 
+  const onClickReset = useCallback(
+    (e) => {
+      e.preventDefault(); // リロードを防ぐ
+      resetMinLikesCount();
+    },
+    [resetMinLikesCount]
+  );
+
+  // todo: コンポーネントの細分化
   return (
     <div className="max-w-5xl mx-auto px-12">
       <form className="mt-12 mb-4">
@@ -22,12 +35,19 @@ const ArticlesBySearchHistoryWord: React.VFC = () => {
           LGTM 数で絞り込み（下限を指定）
         </label>
         <input
-          className="shadow appearance-none border rounded py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded py-2 px-3 mr-2 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="number"
           min="0"
           value={minLikesCount}
           onChange={(e) => setMinLikeCount(parseInt(e.target.value) || 0)}
         />
+        <button
+          className={`${buttonColor} text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+          onClick={onClickReset}
+          disabled={minLikesCount === 0}
+        >
+          リセット
+        </button>
       </form>
       {searchWord && (
         <p className="mb-4 text-xl">
@@ -37,7 +57,6 @@ const ArticlesBySearchHistoryWord: React.VFC = () => {
           </span>
         </p>
       )}
-      {/*todo: lgtm数の下限で絞り込む*/}
       {articleList?.map((article) => {
         return <ArticleItem key={article.id} article={article} />;
       })}
