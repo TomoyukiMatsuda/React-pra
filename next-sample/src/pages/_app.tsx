51d09e899e32c12333b7bcf449eb6704bd4dec43
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-10-08 11:43:11 +0900

    NextNProgressでページ遷移時のプログレスバー表示

--- next-sample/src/pages/_app.tsx
+++ next-sample/src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // <RecoilRoot>で囲うことで、その中でRecoilが利用できるようになる
     <RecoilRoot>
+      <NextNProgress />
       <Component {...pageProps} />
     </RecoilRoot>
   );
