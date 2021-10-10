3fcd5958d3bd1ed63783ae818c42606ca35db2c9
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-10-11 08:18:44 +0900

    const と再代入

--- ts-app-pra/src/Sample2.tsx
+++ ts-app-pra/src/Sample2.tsx
@@ -1,3 +1,4 @@
+// 変数
 export const Sample2 = () => {
   // プリミティブ型のコピーはコピー元に影響を及ぼさない
   let a = "こんにちは";
@@ -20,6 +21,17 @@ export const Sample2 = () => {
     property: "おはよう",
   };
   console.log("d", d);
+
+  // const は再代入を禁止するもの
+  const e = {
+    property: "初期値",
+  };
+  // 値への参照は変更が可能
+  e.property = "初期値を変更";
+  // Attempt to assign to const or readonly variable
+  // Cannot assign to 'e' because it is a constant.
+  // e = {}; // エラーこれはできない
+  // e = d; // これも同様のエラーでできない
 
   return <div>サンプル２</div>;
 };
