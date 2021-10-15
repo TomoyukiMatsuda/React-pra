b14d8a6945651b1599d13cc1808fb2aa479f0d40
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-10-15 18:08:25 +0900

    nextjs-progressbar をアンインストール

--- next-sample/src/pages/_app.tsx
+++ next-sample/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import NextNProgress from "nextjs-progressbar";
 import nProgress from "nprogress";
 
 nProgress.configure({ speed: 200, minimum: 0.3 });
@@ -10,8 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     // <RecoilRoot>で囲うことで、その中でRecoilが利用できるようになる
     <RecoilRoot>
-      <NextNProgress />
-      {/*ページ遷移時とAPIローディング時にプログレスバーを表示*/}
+      <Nextn
       <Component {...pageProps} />
     </RecoilRoot>
   );
