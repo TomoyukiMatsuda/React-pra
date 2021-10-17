52aef4a435c608bd4618646d25dc3e1fa6bf9ca2
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-10-17 21:34:01 +0900

    thisの関数コンテキストからの参照

--- ts-app-pra/src/Sample4.tsx
+++ ts-app-pra/src/Sample4.tsx
@@ -1,3 +1,12 @@
 export const Sample4 = () => {
+  console.log("########サンプル４############");
+
+  const person = {
+    name: "Tomoyuki",
+    hello: function () {
+      console.log("Hello, " + this.name);
+    },
+  };
+  person.hello();
   return <div>サンプル4</div>;
 };
