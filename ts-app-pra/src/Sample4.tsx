5eef6505b344675a599d82450e964be06c00bd25
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-10-18 18:02:02 +0900

    thisはfunctionを読んだ時の「.」の前についているオブジェクトを指している

--- ts-app-pra/src/Sample4.tsx
+++ ts-app-pra/src/Sample4.tsx
@@ -1,12 +1,17 @@
 export const Sample4 = () => {
   console.log("########サンプル４############");
 
+  // this は呼び出し元のオブジェクトへの参照を保持する
   const person = {
     name: "Tomoyuki",
     hello: function () {
       console.log("Hello, " + this.name);
+      console.log(this);
     },
   };
+  console.log(this);
+  // this はfunctionを読んだ時の「.」の前についているオブジェクトを指している（つまり今回の場合は person が this の正体）
   person.hello();
+
   return <div>サンプル4</div>;
 };
