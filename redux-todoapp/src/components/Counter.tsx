84707714db5f5d2b446f4a054fbd1884fd50da6c
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-03 14:33:01 +0900

    Counterコンポーネントを作成

---
+++ redux-todoapp/src/components/Counter.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+import { Count } from "../store/counter/types";
+import {
+  decrementAction,
+  incrementAction,
+  resetAction,
+} from "../store/counter/actions";
+
+export const Counter: React.VFC = () => {
+  const dispatch = useDispatch();
+
+  // actionを発行する関数
+  // 引数にはaction creatorを渡す
+  const handleIncrement = useCallback(() => dispatch(incrementAction()), []);
+  const handleDecrement = useCallback(() => dispatch(decrementAction()), []);
+  const handleReset = useCallback(() => dispatch(resetAction()), []);
+
+  // storeからstateを取得する
+  // rootReducer.counterにcountReducer
+
+  // useSelector(利用するstateの値を返す関数)
+  const currentCount: Count = useSelector((state: RootState) => state.counter);
+
+  return (
+    <>
+      <div>{currentCount.value}</div>
+      <button onClick={handleIncrement}>カウントアップ</button>
+      <button onClick={handleDecrement}>カウントダウン</button>
+      <button onClick={handleReset}>カウントリセット</button>
+    </>
+  );
+};
