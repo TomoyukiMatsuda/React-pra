d71c78a88ffafb18f8653db9dd185b20c8ba292b
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-04 11:07:39 +0900

    todoReduerを作成してrootReducerに結合した

--- redux-todoapp/src/store/index.ts
+++ redux-todoapp/src/store/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers, createStore } from "redux";
 import { countReducer } from "./counter/reducer";
+import { todoReducer } from "./todo/reducer";
 
 // Reducerを増やすときはここに追加していく
 const rootReducer = combineReducers({
   counter: countReducer,
+  todo: todoReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ redux-todoapp/src/store/todo/reducer.ts
@@ -0,0 +1,23 @@
+import { TodoActionTypes, Todos } from "./types";
+import { ActionTypes } from "../actionTypes";
+
+// デフォルト値
+const initialState: Todos = [];
+
+export const todoReducer = (state = initialState, action: TodoActionTypes) => {
+  const latestId = state.length;
+
+  switch (action.type) {
+    case ActionTypes.addTodo:
+      state.push({
+        id: latestId + 1,
+        text: action.payload.text,
+      });
+      return state;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo) => todo.id !== action.payload.id);
+    default:
+      const _: never = action; // これは必要？
+      return state;
+  }
+};
