2e03c7f42ccc8f4630fa4ab21a33d6806b698f24
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-07 10:23:03 +0900

    reducerを作成

--- modal-app/src/store/modal/reducer.ts
+++ modal-app/src/store/modal/reducer.ts
@@ -1,6 +1,27 @@
-import { Modal } from "./types";
+import { Modal, ModalActionTypes } from "./types";
+import { ActionTypes } from "../actionTypes";
 
 const initialState: Modal = {
   isOpen: false,
   content: null,
+};
+
+export const modalReducer = (
+  state = initialState,
+  action: ModalActionTypes
+) => {
+  switch (action.type) {
+    case ActionTypes.openModal:
+      return {
+        isOpen: true,
+        content: state.content,
+      };
+    case ActionTypes.closeModal:
+      return {
+        isOpen: false,
+        content: null,
+      };
+    default:
+      return state;
+  }
 };
