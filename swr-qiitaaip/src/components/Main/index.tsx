03e1c2b0c9bcabb4c5ba6fe584e078979fc98d7b
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-08 09:39:09 +0900

    swrを利用してapiを叩く

--- swr-qiitaaip/src/components/Main/index.tsx
+++ swr-qiitaaip/src/components/Main/index.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import styles from "../../../styles/Home.module.css";
+import useSWR from "swr";
+
+interface Res {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 // fetcher
-const fetcher = async (input: RequestInfo, init?: RequestInit)=> {
-  const res = await fetch(input, init)
-  return res.json()
-}
+const fetcher = async (input: RequestInfo, init?: RequestInit) => {
+  const res = await fetch(input, init);
+  return res.json();
+};
 
-export const Main: React.VFC= () => {
+export const Main: React.VFC = () => {
+  const { data, error } = useSWR<Res[]>(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  if (error) return <div>エラーーーーーー</div>;
+  if (!data) return <div>ローディングちゅう</div>;
+
   return (
     <main className={styles.main}>
-
+      {data.map((d) => (
+        <p key={d.id}>{d.title}</p>
+      ))}
     </main>
-  )
-}
+  );
+};
