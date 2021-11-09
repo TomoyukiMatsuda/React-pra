2d5de142d0a9b16e3fd7ee15d5f67e18f33a48aa
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-09 11:01:24 +0900

    useSWRConfig()を利用してcacheを確認してみた

--- swr-qiitaaip/src/components/Main/index.tsx
+++ swr-qiitaaip/src/components/Main/index.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import styles from "../../../styles/Home.module.css";
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import { JsonPlaceholderResponse } from "../../types/JsonPlaceholder";
 
 export const Main: React.VFC = () => {
-  const { data, error } = useSWR<JsonPlaceholderResponse[]>(
+  const { data: jsonPosts, error } = useSWR<JsonPlaceholderResponse[]>(
     "https://jsonplaceholder.typicode.com/posts"
   );
 
+  // キャッシュの中身を確認してみた
+  // const { cache } = useSWRConfig();
+  // console.log("キャッシュ", cache);
+
   if (error) return <div>エラーーーーーー</div>;
-  if (!data) return <div>ローディングちゅう</div>;
+  if (!jsonPosts) return <div>ローディングちゅう</div>;
 
   return (
     <main className={styles.main}>
       <ol>
-        {data.map((d) => (
-          <li key={d.id}>{d.title}</li>
+        {jsonPosts.map((post) => (
+          <li key={post.id}>{post.title}</li>
         ))}
       </ol>
     </main>
