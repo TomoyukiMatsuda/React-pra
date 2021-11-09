ce83bbfc68c08400bee7d044d092ea6c0ae660e5
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-09 11:34:14 +0900

    qiitaApiを利用して値を取得

--- swr-qiitaaip/src/components/Main/index.tsx
+++ swr-qiitaaip/src/components/Main/index.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import styles from "../../../styles/Home.module.css";
 import useSWR, { useSWRConfig } from "swr";
 import { JsonPlaceholderResponse } from "../../types/JsonPlaceholder";
+import { QiitaItemResponse } from "../../types/Qiita";
 
 export const Main: React.VFC = () => {
+  // jsonDataとqiita どちらを表示させるべきかどうかをハンドリング
   const { data: jsonPosts, error } = useSWR<JsonPlaceholderResponse[]>(
     "https://jsonplaceholder.typicode.com/posts"
+  );
+  const { data: qiitaItems } = useSWR<QiitaItemResponse[]>(
+    "https://qiita.com/api/v2/items?per_page=25&query=react"
   );
 
   // キャッシュの中身を確認してみた
   // const { cache } = useSWRConfig();
   // console.log("キャッシュ", cache);
+
+  console.log("qiitaitems", qiitaItems);
 
   if (error) return <div>エラーーーーーー</div>;
   if (!jsonPosts) return <div>ローディングちゅう</div>;
@@ -18,8 +25,11 @@ export const Main: React.VFC = () => {
   return (
     <main className={styles.main}>
       <ol>
-        {jsonPosts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+        {/*{jsonPosts.map((post) => (*/}
+        {/*  <li key={post.id}>{post.title}</li>*/}
+        {/*))}*/}
+        {qiitaItems?.map((item) => (
+          <li key={item.id}>{item.title}</li>
         ))}
       </ol>
     </main>
