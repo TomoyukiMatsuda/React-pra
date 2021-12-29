fe9eb24fdc6ef23f57ae099890c06a4dc8c95953
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-12-30 08:04:09 +0900

    依存関係を追加

---
+++ techpit-redux-kanban/.ts-cache/index.js
@@ -0,0 +1,3 @@
+var _a;
+const message = (_a = document.querySelector("h1")) === null || _a === void 0 ? void 0 : _a.textContent;
+console.log(message);
---
+++ techpit-redux-kanban/src/index.ts
@@ -0,0 +1,3 @@
+const message = document.querySelector("h1")?.textContent
+
+console.log(message)
