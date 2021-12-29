3974437df25cf49969b33c0f40f207ac1ebbbcc6
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-12-30 08:12:48 +0900

    react を開始

--- techpit-redux-kanban/src/index.ts
+++
@@ -1,3 +0,0 @@
-const message = document.querySelector("h1")?.textContent
-
-console.log(message)
---
+++ techpit-redux-kanban/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <h1>ハロー React!!!</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
