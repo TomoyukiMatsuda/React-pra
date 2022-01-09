9c7fb510a8232d151f62bdc93e4b2b214305c2dd
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2022-01-09 10:04:08 +0900

    カード並べ替え処理にimmerを利用してイミュータブルに

--- techpit-redux-kanban/src/App.tsx
+++ techpit-redux-kanban/src/App.tsx
@@ -2,9 +2,14 @@ import { Header as _Header } from './Header'
 import styled from 'styled-components'
 import React, { useState } from 'react'
 import { Column } from './Column'
+import produce from 'immer'
 
 export const App: React.VFC = () => {
   const [filterValue, setFilterValue] = useState('')
+  /**
+   * 表示位置を調整する「ある id の Card を別の id の Card の手前に挿入する」と具体化すると、
+   *  2 つの id を state columns の持ち主である App が管理する
+   */
   const [columns, setColumns] = useState([
     {
       id: 'A',
@@ -34,57 +39,50 @@ export const App: React.VFC = () => {
       cards: [{ id: 'f', text: '布団から出る (:3っ)っ -=三[＿＿]' }],
     },
   ])
-  // App で保持するドラッグ中のID
+  // ドラッグ中の Card の id を state として保持
   const [draggingCardID, setDraggingCardID] = useState<string | undefined>(
     undefined,
   )
-  // ドロップ処理関数
+  // ドロップ処理関数、 引数toIDはドロップ先の Card の id
   const dropCardTo = (toID: string) => {
     // ドロップ中のID
     const fromID = draggingCardID
     if (!fromID) return
     setDraggingCardID(undefined) // ドラッグ中IDリセット
-
     if (fromID === toID) return // IDが一緒ならばリターン
 
-    setColumns(prevColumns => {
-      // ドラッグ中のcard
-      const card = columns.flatMap(col => col.cards).find(c => c.id === fromID)
-      if (!card) {
-        // fromIDと同じcardが存在しなければ、既存のstateをセット
-        return prevColumns
-      }
+    type Columns = typeof columns
+    setColumns(
+      produce((columns: Columns) => {
+        // ドラッグ中のcardを取得
+        const card = columns
+          .flatMap(col => col.cards)
+          .find(c => c.id === fromID)
+        if (!card) return
 
-      return prevColumns.map(column => {
-        let newColumn = column
+        // 移動元のcolumn
+        const fromColumn = columns.find(col =>
+          col.cards.some(c => c.id === fromID),
+        )
+        if (!fromColumn) return
+        // 移動元のcolumnから移動するcard以外をセット
+        fromColumn.cards = fromColumn.cards.filter(c => c.id !== fromID)
 
-        if (newColumn.cards.some(col => col.id === fromID)) {
-          // columnにfromIDと同じidのcardが存在すれば
-          newColumn = {
-            ...newColumn,
-            cards: newColumn.cards.filter(c => c.id !== fromID), // cardsの値をフィルタリングしてセット
-          }
+        // 移動先のcolumn
+        const toColumn = columns.find(
+          // todo: toIDは 移動先columnのid or 移動先columnにあるcardのid てこと??
+          col => col.id === toID || col.cards.some(card => card.id === toID),
+        )
+        if (!toColumn) return
+
+        let index = toColumn.cards.findIndex(card => card.id === toID)
+        if (index < 0) {
+          index = toColumn.cards.length
         }
-
-        if (newColumn.id === toID) {
-          // 列の末尾に移動の場合 最後にセット
-          newColumn = {
-            ...newColumn,
-            cards: [...newColumn.cards, card],
-          }
-        } else if (newColumn.cards.some(c => c.id === toID)) {
-          // 列の末尾以外に移動の場合、手前の位置にセット
-          newColumn = {
-            ...newColumn,
-            cards: newColumn.cards.flatMap(c =>
-              c.id === toID ? [card, c] : [c],
-            ),
-          }
-        }
-
-        return newColumn
-      })
-    })
+        // 取得したcardをその順番に挿入する
+        toColumn.cards.splice(index, 0, card)
+      }),
+    )
   }
 
   return (
@@ -100,7 +98,7 @@ export const App: React.VFC = () => {
               filterValue={filterValue}
               cards={cards}
               onCardDragStart={cardID => setDraggingCardID(cardID)}
-              onCardDrop={entered => dropCardTo(entered ?? columnID)}
+              onCardDrop={entered => dropCardTo(entered ?? columnID)} // todo: columnのidが渡る可能性もあるの？
             />
           ))}
         </HorizontalScroll>
