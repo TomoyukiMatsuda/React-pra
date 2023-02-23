a52d2420476780478752261e8d378de01291b55c
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2023-02-23 09:50:05 +0900

    add: react

---
+++ otamesiproject/src/pages/swrInfinite/index.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export default function SwrInfinite() {
+  const x = () => {
+
+  }
+
+  return (
+    <Container>
+      <Item />
+    </Container>
+  )
+}
+
+const Container = styled.div``
+const Item = styled.div`
+  height: 240px;
+  width: 600px;
+`
