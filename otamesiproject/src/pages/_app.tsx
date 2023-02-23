48add6eed800870eb7ecc176e6a2854814b10932
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2023-02-23 10:16:22 +0900

    update

--- otamesiproject/src/pages/_app.tsx
+++ otamesiproject/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import "@/src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@another_works/react-landscape";
 import GlobalStyle from "@/src/styles/grobalStyle";
